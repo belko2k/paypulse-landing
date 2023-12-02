bcae08c48786f564fb07b48072fdf6225a7e5297
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2023-12-02 17:12:35 +0100

    desktop header + components

---
+++ src/data.ts
@@ -0,0 +1,22 @@
+export const primaryNavLinks = [
+  {
+    name: 'Features',
+    hash: '#features',
+  },
+  {
+    name: 'Benefits',
+    hash: '#benefits',
+  },
+  {
+    name: 'Testimonial',
+    hash: '#testimonial',
+  },
+  {
+    name: 'FAQ',
+    hash: '#FAQ',
+  },
+  {
+    name: 'News',
+    hash: '#news',
+  },
+] as const;
